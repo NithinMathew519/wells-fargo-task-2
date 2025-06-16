cc96e674c50b90944decf0474c54df3a5d9092e1
NithinMathew519 <nithinmathew519@gmail.com>
2025-06-16 18:21:46 -0500

    Implement data model entities and update configuration for Task 2

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,4 +1,71 @@
 package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long clientId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String contactInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private FinancialAdvisor advisor;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<Portfolio> portfolios;
+
+    protected Client() {
+    }
+
+    public Client(String name, String contactInfo, FinancialAdvisor advisor) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.advisor = advisor;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public FinancialAdvisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(FinancialAdvisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public List<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(List<Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
 }
--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,4 +1,47 @@
 package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long portfolioId;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client) {
+        this.client = client;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
 }
